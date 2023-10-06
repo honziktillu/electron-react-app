335544e01479f6e59f336b2ce5cee1cf2a28f016
honzik <till@spsmb.cz>
2023-10-06 12:10:44 +0200

    fix respon

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import AppRoutes from "./pages/AppRoutes";
 
 function render() {
-    const root = createRoot(document.getElementById("app"));
+    const root = createRoot(document.getElementById("page-content"));
     root.render(<AppRoutes />);
 }
 
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,11 @@ if (require('electron-squirrel-startup')) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
+    width: 1280,
+    height: 720,
+    minWidth: 720,
+    minHeight: 720,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
